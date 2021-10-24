836788453d342610e6684902745ef444569b1329
Joseph <joseph@rubikpay.net>
2021-10-24 19:39:36 +0100

    code update

--- server.js
+++ server.js
@@ -42,11 +42,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/accion'));
+app.use(express.static('./dist/user-search'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile('index.html', {root: '/dist/accion/'});
+res.sendFile('index.html', {root: '/dist/user-search/'});
 });
 
 // Start the app by listening on the default Heroku port
